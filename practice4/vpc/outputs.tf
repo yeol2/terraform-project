925b9488008e97c63ffe4faffd5673069df5ef1b
rowan <qkrdufdl358@naver.com>
2025-03-21 15:45:27 +0900

    test #8

--- practice4/vpc/outputs.tf
+++ practice4/vpc/outputs.tf
@@ -1,6 +1,6 @@
 output "alb_dns_name" {
-    value = aws_lb.webserver_alb.dns_name
-    description = "The domain name of the load balancer"
+  value       = aws_lb.webserver_alb.dns_name
+  description = "The domain name of the load balancer"
 }
 output "deployed_version" {
   value       = "v1.0.0"
--- practice4/vpc/variables.tf
+++ practice4/vpc/variables.tf
@@ -1,33 +1,33 @@
 variable "project_name" {
-    description = "The name of the Terraform practice project"
-    type        = string
-    default     = "terraform-practice"
+  description = "The name of the Terraform practice project"
+  type        = string
+  default     = "terraform-practice"
 }
 variable "vpc_main_cidr" {
-        description = "VPC main CIDR block"
-        default = "10.0.0.0/23"
+  description = "VPC main CIDR block"
+  default     = "10.0.0.0/23"
 }
 variable "server_port" {
-        description = "Webserver's HTTP port"
-        type = number
-        default = 80
+  description = "Webserver's HTTP port"
+  type        = number
+  default     = 80
 }
 
 variable "my_ip" {
-        description = "My public IP"
-        type = string
-        default = "0.0.0.0/0"
+  description = "My public IP"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
 # ALB-sg-name
 variable "alb_security_group_name" {
-        description = "The name of the ALB's security group"
-        type = string
-        default = "webserver-alb-sg-studentN"
+  description = "The name of the ALB's security group"
+  type        = string
+  default     = "webserver-alb-sg-studentN"
 }
 # ALB-name
 variable "alb_name" {
-        description = "The name of the ALB"
-        type = string
-        default = "webserver-alb-studentN"
+  description = "The name of the ALB"
+  type        = string
+  default     = "webserver-alb-studentN"
 }
