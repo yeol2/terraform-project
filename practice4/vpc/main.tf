ebae24776312b83cd0f98d1034639ff0bd0b5b3e
rowan <qkrdufdl358@naver.com>
2025-03-21 15:41:42 +0900

    test #6

--- practice4/vpc/main.tf
+++ practice4/vpc/main.tf
@@ -157,6 +157,6 @@ terraform {
                 key = "vpc/terraform.tfstate"
                 region = "ap-northeast-2"
                 encrypt = true
-                use_lockfile = true
+                # use_lockfile = true
         }
 }
