c991622f1e0117ba74fa14b0a0ab2e0a21ed5240
rowan <qkrdufdl358@naver.com>
2025-03-26 13:44:05 +0900

    dev #3-2

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,34 +1,62 @@
+# 데이터베이스 서브넷 출력
+output "db-az1" {
+  value = aws_subnet.db-az1  # AZ1에 위치한 DB 서브넷 정보 출력
+}
+
+output "db-az2" {
+  value = aws_subnet.db-az2  # AZ2에 위치한 DB 서브넷 정보 출력
+}
+
+# VPC 정보 출력
+output "network-vpc" {
+  value = aws_vpc.aws-vpc  # 생성된 VPC 전체 정보 출력
+}
+
+# 퍼블릭 서브넷 출력
+output "public-az1" {
+  value = aws_subnet.public-az1  # AZ1 퍼블릭 서브넷 정보 출력
+}
+
+output "public-az2" {
+  value = aws_subnet.public-az2  # AZ2 퍼블릭 서브넷 정보 출력
+}
+
+# 서비스 서브넷 출력
+output "service-az1" {
+  value = aws_subnet.service-az1  # AZ1 서비스 서브넷 정보 출력
+}
+
+output "service-az2" {
+  value = aws_subnet.service-az2  # AZ2 서비스 서브넷 정보 출력
+}
+
+# VPC ID 출력
 output "vpc_id" {
-  value       = aws_vpc.aws-vpc.id
-  description = "The ID of the VPC"
+  value = aws_vpc.aws-vpc.id  # VPC의 ID 출력
 }
 
+# VPC의 CIDR 블록 출력
 output "vpc_cidr" {
-  value = aws_vpc.aws-vpc.cidr_block
-  description = "VPC CIDR"
+  value = aws_vpc.aws-vpc.cidr_block  # VPC의 CIDR 블록 출력
 }
 
-output "public_subnet_ids" {
-  value       = [aws_subnet.public-az1.id, aws_subnet.public-az2.id]
-  description = "IDs of public subnets"
+# NAT
+output "nat_ip_az1" {
+  value = aws_eip.nat-eip-az1.public_ip
+}
+output "nat_ip_az2" {
+  value = aws_eip.nat-eip-az2.public_ip
 }
 
-output "service_subnet_ids" {
-  value       = [aws_subnet.service-az1.id, aws_subnet.service-az2.id]
-  description = "IDs of service subnets"
+output "nat_id_az1" {
+  value = aws_nat_gateway.vpc-nat-az1.id
 }
 
-output "db_subnet_ids" {
-  value       = [aws_subnet.db-az1.id, aws_subnet.db-az2.id]
-  description = "IDs of database subnets"
+output "nat_id_az2" {
+  value = aws_nat_gateway.vpc-nat-az2.id
 }
 
-output "nat_gateway_id" {
-  value       = [aws_nat_gateway.vpc-nat-az1.id, aws_nat_gateway.vpc-nat-az2.id]
-  description = "NAT Gateway ID"
-}
-
-output "igw_id" {
-  value       = aws_internet_gateway.vpc-igw.id
-  description = "Internet Gateway ID"
+# DB
+output "db_rt_az1_id" {
+  value = aws_route_table.aws-rt-db.id
 }
